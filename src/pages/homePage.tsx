8991cd3a6c38ded577f2794cddf322788f1b2323
Witnney <witnneygabriel13@gmail.com>
2024-07-01 19:45:17 -0300

    feat: add banner to home page

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,3 +1,13 @@
+import img from "../assets/banner-desconto.png";
+
 export function HomePage() {
-  return <div className="">HomePage</div>;
+  return (
+    <div className="p-5">
+      <img
+        src={img}
+        alt="Até 55% de desconto esse mês"
+        className="h-auto w-full"
+      />
+    </div>
+  );
 }
