48ce5a7c994c9c948e770acc52011af47ff0530d
Witnney <witnneygabriel13@gmail.com>
2024-07-02 19:07:22 -0300

    feat: add utils interface types

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export interface CategoryItem {
+  id: string;
+  name: string;
+  slug: string;
+  imageUrl: string;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  slug: string;
+  description: string;
+  basePrice: number;
+  imageUrls: string[];
+  categoryId: string;
+  discountPercentage: number;
 }
