5dfa8d6ad55c1968e5e25d5995a7071d93e99867
Witnney <witnneygabriel13@gmail.com>
2024-07-03 20:20:04 -0300

    feat: fix name categoryitemtype

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export interface CategoryItem {
+export interface CategoryItemType {
   id: string;
   name: string;
   slug: string;
