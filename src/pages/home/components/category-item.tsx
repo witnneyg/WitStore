b4118192cff0cfd4d36cad1e7ac223bef4136f73
Witnney <witnneygabriel13@gmail.com>
2024-07-03 22:10:56 -0300

    feat: ensure user goes to category page on category item click

--- src/pages/home/components/category-item.tsx
+++ src/pages/home/components/category-item.tsx
@@ -1,15 +1,18 @@
 import { Badge } from "@/components/ui/badge";
 import { CATEGORY_ICON } from "@/constants/category-icon";
 import { CategoryItemType } from "@/lib/utils";
+import { Link } from "react-router-dom";
 
 export function CategoryItem(category: CategoryItemType) {
   return (
-    <Badge
-      variant="outline"
-      className="flex justify-center items-center py-3 gap-2 rounded-lg"
-    >
-      {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
-      <span className="font-bold text-xs">{category.name}</span>
-    </Badge>
+    <Link to={`/category/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="flex justify-center items-center py-3 gap-2 rounded-lg"
+      >
+        {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+        <span className="font-bold text-xs">{category.name}</span>
+      </Badge>
+    </Link>
   );
 }
