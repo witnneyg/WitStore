1ce2cf43591973f8f90d5c5d576136f16192847e
Witnney <witnneygabriel13@gmail.com>
2024-07-05 14:49:18 -0300

    feaT: ensure recommend products are shown on product details page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import { Categories } from "./components/categories";
-import { ProductList } from "./components/product-list";
+import { ProductList } from "../../components/ui/product-list";
 import { Product } from "@/lib/utils";
 import { useState } from "react";
 
