eda7db51abf139236dd88117dff1b803f1e7c739
Witnney <witnneygabriel13@gmail.com>
2024-07-07 20:34:36 -0300

    feat: ensure cart products can be scrolled

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -5,12 +5,14 @@ import { useContext } from "react";
 import { CartItem } from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
 
 export function Cart() {
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 h-full">
       <Badge
         className="w-fit gap-1 text-base uppercase border-2 border-primary px-3 py-[0.375rem]"
         variant="outline"
@@ -19,19 +21,23 @@ export function Cart() {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <CartItem
-              key={product.id}
-              product={computeProductTotalPrice(product as any) as any}
-            />
-          ))
-        ) : (
-          <p className="text-center font-semibold">
-            Carrinho vazio. Vamos fazer compras?
-          </p>
-        )}
+      <div className="flex flex-col gap-5 h-full overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-8">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  key={product.id}
+                  product={computeProductTotalPrice(product as any) as any}
+                />
+              ))
+            ) : (
+              <p className="text-center font-semibold">
+                Carrinho vazio. Vamos fazer compras?
+              </p>
+            )}
+          </div>
+        </ScrollArea>
       </div>
 
       <div className="flex flex-col gap-3">
@@ -62,6 +68,8 @@ export function Cart() {
           <p>Total</p>
           <p>R$ {total.toFixed(2)}</p>
         </div>
+
+        <Button className="uppercase font-bold mt-7">Finalizar compra</Button>
       </div>
     </div>
   );
