fefa44cb1fbb493ec6e432d2ee57c18a46b5160c
Witnney <witnneygabriel13@gmail.com>
2024-08-02 14:51:30 -0300

    feat: add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { config } from "dotenv";
 import getCategory from "./controllers/category.js";
 import getProduct from "./controllers/product.js";
@@ -8,10 +9,11 @@ const port = 8888;
 
 config();
 
+app.use(cors());
 app.use(express.json());
 
-app.use("/category", getCategory);
-app.use("/product", getProduct);
+app.use("/categories", getCategory);
+app.use("/products", getProduct);
 
 app.listen(port, () => {
   console.log(`Rodando na porta! ${port}`);
