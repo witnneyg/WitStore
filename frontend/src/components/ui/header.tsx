6efbfa94b1e502b21440e78bb95487275d3c611e
Witnney <witnneygabriel13@gmail.com>
2024-08-05 21:02:01 -0300

    feat: add banner to desktop

--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -21,81 +21,91 @@ import { Cart } from "./cart";
 
 export function Header() {
   return (
-    <Card className="flex justify-between items-center p-[1.875rem]">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button size="icon" variant="outline">
-            <MenuIcon />
-          </Button>
-        </SheetTrigger>
-
-        <SheetContent side="left">
-          <SheetHeader className="text-left text-lg font-semibold">
-            Menu
-          </SheetHeader>
-
-          <div className="flex flex-col gap-3 mt-2">
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <LogInIcon size={16} />
-              Fazer Login
+    <Card className="flex justify-between items-center p-[1.875rem]  ">
+      <div className="sm:hidden">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
             </Button>
+          </SheetTrigger>
 
-            <SheetClose asChild>
-              <Link to="/">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <HomeIcon size={16} />
-                  Início
-                </Button>
-              </Link>
-            </SheetClose>
+          <SheetContent side="left">
+            <SheetHeader className="text-left text-lg font-semibold">
+              Menu
+            </SheetHeader>
 
-            <SheetClose asChild>
-              <Link to="/orders">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <PackageSearchIcon size={16} />
-                  Meus Pedidos
-                </Button>
-              </Link>
-            </SheetClose>
+            <div className="flex flex-col gap-3 mt-2">
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <LogInIcon size={16} />
+                Fazer Login
+              </Button>
 
-            <SheetClose asChild>
-              <Link to="/deals">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <PercentIcon size={16} />
-                  Ofertas
-                </Button>
-              </Link>
-            </SheetClose>
+              <SheetClose asChild>
+                <Link to="/">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <HomeIcon size={16} />
+                    Início
+                  </Button>
+                </Link>
+              </SheetClose>
 
-            <SheetClose asChild>
-              <Link to={`/catalog`}>
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <ListOrderedIcon size={16} />
-                  Catálogo
-                </Button>
-              </Link>
-            </SheetClose>
-          </div>
-        </SheetContent>
-      </Sheet>
+              <SheetClose asChild>
+                <Link to="/orders">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <PackageSearchIcon size={16} />
+                    Meus Pedidos
+                  </Button>
+                </Link>
+              </SheetClose>
+
+              <SheetClose asChild>
+                <Link to="/deals">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <PercentIcon size={16} />
+                    Ofertas
+                  </Button>
+                </Link>
+              </SheetClose>
+
+              <SheetClose asChild>
+                <Link to={`/catalog`}>
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <ListOrderedIcon size={16} />
+                    Catálogo
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
 
       <Link to="/">
         <h1 className="text-lg font-semibold">
           <span className="text-primary">Wit </span>Store
         </h1>
       </Link>
+
+      <div className="sm:flex items-center gap-6 font-semibold hidden">
+        <a href="">Início</a>
+        <a href="" className="border-x-2 border-x-zinc-800 px-4">
+          Catálogo
+        </a>
+        <a href="">Ofertas</a>
+      </div>
 
       <Sheet>
         <SheetTrigger asChild>
--- frontend/src/pages/home/components/promo-banner.tsx
+++ frontend/src/pages/home/components/promo-banner.tsx
@@ -1,5 +1,13 @@
 import { ImgHTMLAttributes } from "react";
 
-export function PromoBanner({ ...props }: ImgHTMLAttributes<HTMLImageElement>) {
-  return <img className="h-auto w-full px-5" {...props} />;
+export function PromoBanner({
+  className,
+  ...props
+}: ImgHTMLAttributes<HTMLImageElement>) {
+  return (
+    <img
+      className={`h-auto w-full px-5 md:px-0 ${className || ""}`}
+      {...props}
+    />
+  );
 }
