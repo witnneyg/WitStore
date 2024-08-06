70908b89f83200f73fbcd511228f13d9cafd4d04
Witnney <witnneygabriel13@gmail.com>
2024-08-05 21:22:00 -0300

    feat: add fetch to categories

--- frontend/src/pages/home/components/categories.tsx
+++ frontend/src/pages/home/components/categories.tsx
@@ -1,51 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CategoryItem } from "./category-item";
-import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+import { CategoryType } from "@/lib/utils";
 
 export function Categories() {
-  const [category] = useState([
-    {
-      id: uuidv4(),
-      name: "Teclados",
-      slug: "keyboards",
-      imageUrl: "https://fsw-store.s3.sa-east-1.amazonaws.com/keyboards.png",
-    },
-    {
-      id: uuidv4(),
-      name: "Mouses",
-      slug: "mouses",
-      imageUrl: "https://fsw-store.s3.sa-east-1.amazonaws.com/mouses.png",
-    },
-    {
-      id: uuidv4(),
-      name: "Fones",
-      slug: "headphones",
-      imageUrl: "https://fsw-store.s3.sa-east-1.amazonaws.com/headphones.png",
-    },
-    {
-      id: uuidv4(),
-      name: "Mousepads",
-      slug: "mousepads",
-      imageUrl: "https://fsw-store.s3.sa-east-1.amazonaws.com/mousepads.png",
-    },
-    {
-      id: uuidv4(),
-      name: "Monitors",
-      slug: "monitors",
-      imageUrl: "https://fsw-store.s3.sa-east-1.amazonaws.com/monitors.png",
-    },
-    {
-      id: uuidv4(),
-      name: "Speakers",
-      slug: "speakers",
-      imageUrl: "https://fsw-store.s3.sa-east-1.amazonaws.com/speakers.png",
-    },
-  ]);
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+
+  useEffect(() => {
+    async function getCategory() {
+      try {
+        const res = await axios.get("http://localhost:8888/categories");
+
+        setCategories(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getCategory();
+  }, []);
 
   return (
     <div className="grid grid-cols-2 gap-y-2 gap-x-4">
-      {category.map((item) => (
-        <CategoryItem name={item.name} slug={item.slug} key={item.id} />
+      {categories.map((item) => (
+        <CategoryItem name={item.name} slug={item.slug} key={item._id} />
       ))}
     </div>
   );
