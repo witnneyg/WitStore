a1ba08caad221fb32f87aab1133966b5e323dd24
Witnney <witnneygabriel13@gmail.com>
2024-08-05 21:08:26 -0300

    feat: add router productSlug

--- backend/src/controllers/product.js
+++ backend/src/controllers/product.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
-import { getProduct } from "../services/product.js";
+import { getProduct, getProductById } from "../services/product.js";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
   try {
     const product = await getProduct();
+    res.send(product);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/:productSlug", async (req, res) => {
+  try {
+    const product = await getProductById(req.params.productSlug);
     res.send(product);
   } catch (error) {
     res.status(500).send(error);
--- backend/src/services/product.js
+++ backend/src/services/product.js
@@ -3,6 +3,15 @@ import { Product } from "../models/models.js";
 
 export async function getProduct() {
   await databaseConnection();
-  const getProduct = await Product.find();
+
+  const getProduct = await Product.find().populate("category");
+
   return getProduct;
+}
+
+export async function getProductById(productSlug) {
+  await databaseConnection();
+  const getProductBySlug = await Product.findOne({ slug: productSlug });
+
+  return getProductBySlug;
 }
