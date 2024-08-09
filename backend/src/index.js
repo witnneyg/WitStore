a3769fc125fc2e8b2c04ed1f961e456ccdc2cca6
Witnney <witnneygabriel13@gmail.com>
2024-08-09 19:26:06 -0300

    feat: add routes to login and register

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import { config } from "dotenv";
 import getCategory from "./controllers/category.js";
 import getProduct from "./controllers/product.js";
+import registerUser from "./controllers/register-controller.js";
+import loginUser from "./controllers/login-controller.js";
+import { authMiddleware } from "./middleware/authenticate-middleware.js";
 
 const app = express();
 const port = 8888;
@@ -12,8 +15,10 @@ config();
 app.use(cors());
 app.use(express.json());
 
-app.use("/categories", getCategory);
-app.use("/products", getProduct);
+app.use("/categories", authMiddleware, getCategory);
+app.use("/products", authMiddleware, getProduct);
+app.use("/auth", registerUser);
+app.use("/auth", loginUser);
 
 app.listen(port, () => {
   console.log(`Rodando na porta! ${port}`);
