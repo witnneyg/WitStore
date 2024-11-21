c32b54a59897870dcbb3f12e2f44afed81a99be9
Witnney <witnneygabriel13@gmail.com>
2024-11-20 21:47:26 -0300

    feat: add carousel

--- frontend/src/components/ui/product-list.tsx
+++ frontend/src/components/ui/product-list.tsx
@@ -2,19 +2,41 @@ import { ProductItem } from "@/components/ui/product-item";
 import { computeProductTotalPrice } from "@/helpers/product";
 
 import { Product } from "@/lib/utils";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselNext,
+  CarouselPrevious,
+} from "./carousel";
 
 interface ProductListProps {
   products: Product[];
 }
 
 export function ProductList({ products }: ProductListProps) {
+  console.log(products.length);
   return (
-    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden md:gap-5">
-      {products.map((product) => (
-        <div key={product._id} className=" w-[170px] max-w-[170px]">
-          <ProductItem product={computeProductTotalPrice(product)} />
+    <div className="flex w-full  px-5 md:gap-5 relative">
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="max-w-full flex"
+      >
+        <CarouselContent className="flex gap-4 ml-2">
+          {products.map((product) => (
+            <div key={product._id} className=" w-[170px] max-w-[170px]">
+              <ProductItem product={computeProductTotalPrice(product)} />
+            </div>
+          ))}
+        </CarouselContent>
+        <div className="absolute top-20 left-14">
+          <CarouselPrevious />
         </div>
-      ))}
+        <div className="absolute top-20 right-12">
+          <CarouselNext />
+        </div>
+      </Carousel>
     </div>
   );
 }
