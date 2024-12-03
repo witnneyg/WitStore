93b36c513bfbd96c8448d7418161dadc08886755
Witnney <witnneygabriel13@gmail.com>
2024-12-02 21:49:19 -0300

    feat: add stripe checkout endpoint

--- backend/src/controllers/checkout-controller.js
+++ backend/src/controllers/checkout-controller.js
@@ -1,11 +1,40 @@
 import { Router } from "express";
+import Stripe from "stripe";
 
 const router = Router();
 
-router.post("/", (req, res) => {
-  // Lógica para processar a compra
+router.post("/", async (req, res) => {
+  const products = req.body;
+
+  const stripe = new Stripe(process.env.STRIP_SECRET_KEY, {
+    apiVersion: "2024-11-20.acacia",
+  });
+
+  const checkout = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "payment",
+    success_url: "http://localhost:5173",
+    cancel_url: "http://localhost:5173",
+    line_items: products.map((product) => {
+      return {
+        price_data: {
+          currency: "brl",
+          product_data: {
+            name: product.name,
+            description: product.description,
+            images: product.imageUrls,
+          },
+          unit_amount: product.totalPrice * 100,
+        },
+        quantity: product.quantity,
+      };
+    }),
+  });
+
+  console.log(checkout);
+
   try {
-    res.json({ message: "Compra realizada com sucesso!", data: req.body });
+    res.status(200).send({ checkout });
   } catch (error) {
     res.status(500).send({
       message: "Erro ao processar a compra. Por favor, tente novamente.",
