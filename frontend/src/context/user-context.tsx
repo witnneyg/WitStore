78a6950cf66713aa299f918d73bdfc7f66c62888
Witnney <witnneygabriel13@gmail.com>
2024-12-08 19:41:22 -0300

    feat: add UserContext and rename cart to CartContext

---
+++ frontend/src/context/user-context.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { jwtDecode, JwtPayload } from "jwt-decode";
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+export interface CustomJwtPayload extends JwtPayload {
+  email: string;
+  name: string;
+  _id: string;
+}
+
+interface UserContextProps {
+  user: CustomJwtPayload | undefined;
+  setUser: React.Dispatch<React.SetStateAction<CustomJwtPayload | undefined>>;
+}
+
+export const UserContext = createContext<UserContextProps>({
+  user: undefined,
+  setUser: () => {},
+});
+
+export function UserProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<CustomJwtPayload | undefined>(undefined);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decoded = jwtDecode<CustomJwtPayload>(token);
+      setUser(decoded);
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
