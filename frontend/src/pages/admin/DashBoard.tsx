943de0874ef27d54ed61ceff0eb588d9447f3309
Witnney <witnneygabriel13@gmail.com>
2025-01-07 16:20:28 -0300

    feat: consume GET products endpoint

--- frontend/src/pages/admin/DashBoard.tsx
+++ frontend/src/pages/admin/DashBoard.tsx
@@ -1,39 +1,24 @@
 import { Card } from "@/components/ui/card";
+import { Product } from "@/lib/utils";
+import { api } from "@/services/api";
 import { Edit, Trash } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export function DashBoard() {
-  const data = [
-    {
-      id: 1,
-      name: "CPU Fan",
-      price: "57",
-      picture: "https://via.placeholder.com/50",
-    },
-    {
-      id: 2,
-      name: "GPU",
-      price: "399",
-      picture: "https://via.placeholder.com/50",
-    },
-    {
-      id: 2,
-      name: "GPU",
-      price: "399",
-      picture: "https://via.placeholder.com/50",
-    },
-    {
-      id: 1,
-      name: "CPU Fan",
-      price: "57",
-      picture: "https://via.placeholder.com/50",
-    },
-    {
-      id: 2,
-      name: "GPU",
-      price: "399",
-      picture: "https://via.placeholder.com/50",
-    },
-  ];
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const res = await api.get("/products");
+        setProducts(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getProducts();
+  }, []);
 
   return (
     <div className="flex h-full">
@@ -54,16 +39,22 @@ export function DashBoard() {
                 </tr>
               </thead>
               <tbody>
-                {data.map((item, index) => (
-                  <tr key={item.id} className="">
-                    <td className="border  p-2">{index + 1}</td>
-                    <td className="border  p-2">{item.name}</td>
-                    <td className="border  p-2">R$ {item.price}</td>
-                    <td className="border  p-2">
+                {products.map((item) => (
+                  <tr key={item._id} className="">
+                    <td className="border  pl-2">{item._id.slice(0, 8)}</td>
+                    <td className="border  p-2 truncate max-w-20  md:max-w-40">
+                      {item.name}
+                    </td>
+                    <td className="border  p-2">R$ {item.basePrice}</td>
+                    <td className="border  p-2 pl-5 w-2 text-center ">
                       <img
-                        src={item.picture}
+                        src={item.imageUrls[0]}
                         alt={item.name}
                         className="h-10 w-10 object-cover"
+                        onError={(e) => {
+                          (e.target as HTMLImageElement).src =
+                            "https://static-00.iconduck.com/assets.00/image-x-generic-icon-256x256-vemtnuvp.png";
+                        }}
                       />
                     </td>
                     <td className="border">
