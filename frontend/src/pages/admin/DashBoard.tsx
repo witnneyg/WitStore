4e477acb88b4c7367eb33b974f5f761f05dc23b6
Witnney <witnneygabriel13@gmail.com>
2025-01-14 22:13:54 -0300

    feat: ensure product are edited and deleted

--- frontend/src/pages/admin/DashBoard.tsx
+++ frontend/src/pages/admin/DashBoard.tsx
@@ -1,11 +1,26 @@
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Product } from "@/lib/utils";
 import { api } from "@/services/api";
 import { Edit, Trash } from "lucide-react";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 export function DashBoard() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    basePrice: 0,
+  });
 
   useEffect(() => {
     async function getProducts() {
@@ -19,6 +34,42 @@ export function DashBoard() {
 
     getProducts();
   }, []);
+
+  function handleSelectedProduct(product: Product) {
+    setFormData({
+      name: product.name,
+      basePrice: product.basePrice,
+    });
+  }
+
+  function handleChanges(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function handleSaveChanges(itemId: string, e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!formData.name || !formData.basePrice) {
+      alert("Preencha todos os campos obrigatórios.");
+      return;
+    }
+
+    const newProduct = products.map((productItem) =>
+      productItem._id === itemId
+        ? { ...productItem, name: formData.name, basePrice: formData.basePrice }
+        : productItem
+    );
+
+    setProducts(newProduct);
+  }
+
+  function handleDeleteChanges(itemId: string) {
+    const newProduct = products.filter((product) => product._id !== itemId);
+
+    setProducts(newProduct);
+  }
 
   return (
     <div className="flex h-full">
@@ -59,12 +110,89 @@ export function DashBoard() {
                     </td>
                     <td className="border">
                       <div className="flex gap-2 ml-4">
-                        <button className="p-2 bg-blue-500 text-white rounded">
-                          <Edit size={16} />
-                        </button>
-                        <button className="p-2 bg-red-500 text-white rounded">
+                        <Dialog>
+                          <DialogTrigger asChild>
+                            <Button
+                              className="p-2 text-white rounded"
+                              onClick={() => handleSelectedProduct(item)}
+                            >
+                              <Edit size={16} />
+                            </Button>
+                          </DialogTrigger>
+
+                          <DialogContent>
+                            <DialogHeader>
+                              <DialogTitle>Editar Produto</DialogTitle>
+
+                              <DialogDescription className="flex flex-col gap-4">
+                                <form
+                                  onSubmit={(e) =>
+                                    handleSaveChanges(item._id, e)
+                                  }
+                                >
+                                  <div className="flex flex-col gap-2 mt-4">
+                                    <label
+                                      htmlFor="name"
+                                      className="font-medium text-white"
+                                    >
+                                      Nome
+                                    </label>
+                                    <input
+                                      type="text"
+                                      id="name"
+                                      name="name"
+                                      value={formData.name}
+                                      className="mt-1 p-2 border text-white border-black rounded w-full bg-[#252424]"
+                                      onChange={handleChanges}
+                                    />
+                                  </div>
+
+                                  <div className="flex flex-col gap-2">
+                                    <label
+                                      htmlFor="basePrice"
+                                      className="block font-medium text-white"
+                                    >
+                                      Preço
+                                    </label>
+                                    <input
+                                      type="number"
+                                      id="basePrice"
+                                      name="basePrice"
+                                      value={formData.basePrice}
+                                      className="mt-1 p-2 border text-white border-black rounded w-full bg-[#252424]"
+                                      onChange={handleChanges}
+                                    />
+                                  </div>
+
+                                  <DialogFooter>
+                                    <DialogClose
+                                      className="flex justify-end gap-2"
+                                      asChild
+                                    >
+                                      <Button
+                                        type="button"
+                                        className="p-2 rounded"
+                                        variant="destructive"
+                                      >
+                                        Cancelar
+                                      </Button>
+                                    </DialogClose>
+                                    <DialogClose>
+                                      <Button type="submit">Salvar</Button>
+                                    </DialogClose>
+                                  </DialogFooter>
+                                </form>
+                              </DialogDescription>
+                            </DialogHeader>
+                          </DialogContent>
+                        </Dialog>
+                        <Button
+                          variant="destructive"
+                          className="p-2 rounded"
+                          onClick={() => handleDeleteChanges(item._id)}
+                        >
                           <Trash size={16} />
-                        </button>
+                        </Button>
                       </div>
                     </td>
                   </tr>
