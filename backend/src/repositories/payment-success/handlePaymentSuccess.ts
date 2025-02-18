c264857bd12e4716c4c0621c6a6d06d39addd2ff
Witnney <witnneygabriel13@gmail.com>
2025-02-18 16:59:07 -0300

    feat: add payment confirmed status when the payment is successful

--- backend/src/repositories/payment-success/handlePaymentSuccess.ts
+++ backend/src/repositories/payment-success/handlePaymentSuccess.ts
@@ -1,5 +1,6 @@
 import Stripe from "stripe";
 import { IPaymentSuccessRepository } from "../../controllers/payment-success/protocols.js";
+import { Order } from "../../models/orderSchema.js";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2024-11-20.acacia",
@@ -16,6 +17,8 @@ export class PaymentSuccessRepository implements IPaymentSuccessRepository {
       process.env.STRIPE_WEBHOOK_SECRET_KEY
     );
     if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
       const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
         event.data.object.id,
         {
@@ -23,6 +26,16 @@ export class PaymentSuccessRepository implements IPaymentSuccessRepository {
         }
       );
       const lineItems = sessionWithLineItems.line_items.data;
+
+      await Order.findByIdAndUpdate(
+        session.metadata.orderId,
+        {
+          status: "PAYMENT_CONFIRMED",
+        },
+        {
+          new: true,
+        }
+      );
 
       return lineItems;
     }
