0f56088d453b181c5a6cdd854bc0bb239aa1da01
Witnney <witnneygabriel13@gmail.com>
2025-03-01 17:56:17 -0300

    feat: add VITE_API_BASE_URL on axios

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,5 @@
 import axios, { AxiosInstance } from "axios";
 
 export const api: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8888",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8888",
 });
-
-// import.meta.env.VITE_API_BASE_URL ||
