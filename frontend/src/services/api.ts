f87b76115c7c60b707a777da56c053e5aaa2a229
Witnney Gabriel <witnneygabriel13@gmail.com>
2026-02-14 19:02:09 -0300

    commit

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,5 @@
 import axios, { AxiosInstance } from "axios";
 
 export const api: AxiosInstance = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8888",
-  baseURL: "http://localhost:8888",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8888",
 });
